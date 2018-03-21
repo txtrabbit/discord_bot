3f2ceec445f933f6cbcf1a431d4f856546f07ff8
txtrabbit <txtrabbit@gmail.com>
2018-03-21 18:42:55 +0500

    comment

---
+++ Discordbot.php
@@ -0,0 +1,27 @@
+<?php
+
+include __DIR__.'/vendor/autoload.php';
+
+use Discord\Discord;
+
+$dotenv = new Dotenv\Dotenv(__DIR__);
+$dotenv->load();
+
+$discord = new Discord([
+	'token' => getenv('MY_TOKEN'),
+]);
+
+$discord->on('ready', function ($discord) {
+	echo "Bot is ready!", PHP_EOL;
+
+	// Listen for messages.
+$discord->on('message', function ($message, $discord) {
+	echo "{$message->author->username}: {$message->content}",PHP_EOL;
+  $filename = 'somefile.txt';
+  $text = "{$message->author->username}: {$message->content}\n";
+//записываем текст в файл
+file_put_contents($filename, $text, FILE_APPEND);
+	});
+});
+
+$discord->run();
---
+++ test2.php
@@ -0,0 +1,5 @@
+<?php
+$filename = 'somefile.txt';
+$text = 'Text sample.';
+//записываем текст в файл
+file_put_contents($filename, $text);
